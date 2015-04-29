46ca668b7b6ff5102729f9ad47c821922bbf2693
lijunwei <lijunwei@kingsoft.com>
2015-04-29 16:46:21 +0800

    update exceptions

---
+++ src/main/java/com/ksyun/ks3/exception/serviceside/AlgorithmInvalidForCustomerKeyException.java
@@ -0,0 +1,19 @@
+package com.ksyun.ks3.exception.serviceside;
+
+import com.ksyun.ks3.exception.Ks3ServiceException;
+
+/**
+ * @author lijunwei[lijunwei@kingsoft.com]  
+ * 
+ * @date 2015年4月28日 下午8:59:28
+ * 
+ * @description 
+ **/
+public class AlgorithmInvalidForCustomerKeyException extends Ks3ServiceException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3030581260300000170L;
+
+}
